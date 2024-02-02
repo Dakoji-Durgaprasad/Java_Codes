a197bc1b9fd0f7ddcc35d2498167262296daded6
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-02-02 14:08:20 +0530

    Update MiniAtmProject.java

--- MiniAtmProject.java
+++ MiniAtmProject.java
@@ -1,6 +1,7 @@
+
 import java.util.*;
 
-public class MiniAtmProject {
+public class MiniAtm {
 
 	public static void main(String[] args) {
 		
@@ -8,7 +9,7 @@ public class MiniAtmProject {
 		int Deposit = 0;
 		int Withdraw = 0;
 		int password=1234;
-		int a=0;
+		int pin=0;
 		
 		while(true) { 
 		System.out.println("Welcome to ATM \n");
@@ -17,7 +18,8 @@ public class MiniAtmProject {
 		System.out.println("1.Check Balance ");
 		System.out.println("2.Deposit");
 		System.out.println("3.Withdraw");
-		System.out.println("4.EXIT \n");
+		System.out.println("4.Transaction History");
+		System.out.println("5.EXIT \n");
 		
 		Scanner sc = new Scanner(System.in);
 
@@ -27,7 +29,7 @@ public class MiniAtmProject {
 		
 		switch(opt){
 		case 1:
-			if(pin(a)) {
+			if(pin(pin)) {
 			System.out.println("Your current Balance is : "+Balance +"\n");
 			}else {
 				System.out.println("Enter valid Pin!");
@@ -35,7 +37,7 @@ public class MiniAtmProject {
 			break;
 		
 		case 2:
-			if(pin(a)) {
+			if(pin(pin)) {
 			System.out.println("Enter an amount to Deposit: ");
 			Deposit = sc.nextInt();
 			Balance = Deposit + Balance;
@@ -46,7 +48,7 @@ public class MiniAtmProject {
 			break;
 		
 		case 3:
-			if(pin(a)) {
+			if(pin(pin)) {
 			System.out.println("Enter an amount to Withdraw: ");
 			Withdraw = sc.nextInt();
 			if(Balance > Withdraw) {
@@ -62,9 +64,9 @@ public class MiniAtmProject {
 			break;
 		case 4:
 			System.out.println("Your last transactioin history : ");
-			System.out.println("Last time added desposited : "+Deposit);
-			System.out.println("Last time added withdrawal : "+Withdraw);
-			System.out.println("Your current Balance is : "+Balance +"\n");
+			System.out.println("Last time added desposited amount : "+Deposit);
+			System.out.println("Last time taken withdrawal amount : "+Withdraw);
+			System.out.println("Your current available Balance is : "+Balance +"\n");
 			break;
 		case 5:
 			System.out.println("Thanks for Visiting!! \n");
@@ -78,13 +80,13 @@ public class MiniAtmProject {
 		}
 		}
 		}
-		public static boolean pin(int a) {
+		public static boolean pin(int pin) {
 		
 		int password = 1234;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your four digit pin: ");
-		 a = sc.nextInt(); 
-		 return(a==password);
+		pin = sc.nextInt(); 
+		 return(pin==password);
 		}
 	
 	}
